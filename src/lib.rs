49ac6d2c3fac44e58e4304181308d53ac53a34c3
5iddy <siddhuyeturi@gmail.com>
2022-09-16 08:58:17 +0530

    first commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,6 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+// #![warn(missing_docs)]
+// #![warn(rustdoc::missing_crate_level_docs)]
+// #![warn(rustdoc::broken_intra_doc_links)]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod msg;
+pub mod query;
